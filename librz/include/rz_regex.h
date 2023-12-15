3a1be33f303e053e934d613632e7098044a8966b
Rot127 <unisono@quyllur.org>
2023-12-15 12:33:44 -0500

    Replace POSIX regex API with PCRE2 one.

--- librz/include/rz_regex.h
+++ librz/include/rz_regex.h
@@ -4,64 +4,66 @@
 #ifndef RZ_REGEX_H
 #define RZ_REGEX_H
 
+#include <rz_vector.h>
 #include <rz_types.h>
 #include <rz_list.h>
 #include <sys/types.h>
 
-#include <pcre2posix.h>
+// TODO Check for static windows build and define this
+/* #define PCRE2_STATIC */
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
 
-// Maximum number of matches we count.
-#define RZ_REGEX_MAX_MATCH_COUNT 64
-#define RZ_REGEX_MATCH_FAIL      -1
-#define RZ_REGEX_MATCH_OVERFLOW  -2
+// Some basic PCRE2 macros. There are way more defined
+// and should be added here if needed.
+#define RZ_REGEX_ERROR_NOMATCH PCRE2_ERROR_NOMATCH
+#define RZ_REGEX_ERROR_PARTIAL PCRE2_ERROR_PARTIAL
 
-typedef int RzRegexFlags; ///< Regex flag bits.
-typedef regex_t RzRegex; ///< A compiler regex expression.
-typedef regmatch_t RzRegexMatch; ///< A match with its start and end offset.
+#define RZ_REGEX_CASELESS  PCRE2_CASELESS
+#define RZ_REGEX_EXTENDED  PCRE2_EXTENDED
+#define RZ_REGEX_MULTILINE PCRE2_MULTILINE
 
-#define RZ_REGEX_ICASE    REG_ICASE /* Maps to PCRE2_CASELESS */
-#define RZ_REGEX_NEWLINE  REG_NEWLINE /* Maps to PCRE2_MULTILINE */
-#define RZ_REGEX_NOTBOL   REG_NOTBOL /* Maps to PCRE2_NOTBOL */
-#define RZ_REGEX_NOTEOL   REG_NOTEOL /* Maps to PCRE2_NOTEOL */
-#define RZ_REGEX_DOTALL   REG_DOTALL /* NOT defined by POSIX; maps to PCRE2_DOTALL */
-#define RZ_REGEX_NOSUB    REG_NOSUB /* Do not report what was matched */
-#define RZ_REGEX_UTF      REG_UTF /* NOT defined by POSIX; maps to PCRE2_UTF */
-#define RZ_REGEX_STARTEND REG_STARTEND /* BSD feature: pass subject string by so,eo */
-#define RZ_REGEX_NOTEMPTY REG_NOTEMPTY /* NOT defined by POSIX; maps to PCRE2_NOTEMPTY */
-#define RZ_REGEX_UNGREEDY REG_UNGREEDY /* NOT defined by POSIX; maps to PCRE2_UNGREEDY */
-#define RZ_REGEX_UCP      REG_UCP /* NOT defined by POSIX; maps to PCRE2_UCP */
-#define RZ_REGEX_PEND     REG_PEND /* GNU feature: pass end pattern by re_endp */
-#define RZ_REGEX_NOSPEC   REG_NOSPEC /* Maps to PCRE2_LITERAL */
-#define RZ_REGEX_EXTENDED REG_EXTENDED /* Unused by PCRE2 */
+#define RZ_REGEX_UNSET PCRE2_UNSET
 
-/* regerror() flags */
-#define RZ_REGEX_ASSERT   REG_ASSERT /* internal error ? */
-#define RZ_REGEX_BADBR    REG_BADBR /* invalid repeat counts in {} */
-#define RZ_REGEX_BADPAT   REG_BADPAT /* pattern error */
-#define RZ_REGEX_BADRPT   REG_BADRPT /* ? * + invalid */
-#define RZ_REGEX_EBRACE   REG_EBRACE /* unbalanced {} */
-#define RZ_REGEX_EBRACK   REG_EBRACK /* unbalanced [] */
-#define RZ_REGEX_ECOLLATE REG_ECOLLATE /* collation error - not relevant */
-#define RZ_REGEX_ECTYPE   REG_ECTYPE /* bad class */
-#define RZ_REGEX_EESCAPE  REG_EESCAPE /* bad escape sequence */
-#define RZ_REGEX_EMPTY    REG_EMPTY /* empty expression */
-#define RZ_REGEX_EPAREN   REG_EPAREN /* unbalanced () */
-#define RZ_REGEX_ERANGE   REG_ERANGE /* bad range inside [] */
-#define RZ_REGEX_ESIZE    REG_ESIZE /* expression too big */
-#define RZ_REGEX_ESPACE   REG_ESPACE /* failed to get memory */
-#define RZ_REGEX_ESUBREG  REG_ESUBREG /* bad back reference */
-#define RZ_REGEX_INVARG   REG_INVARG /* bad argument */
-#define RZ_REGEX_NOMATCH  REG_NOMATCH /* match failed */
+typedef int RzRegexStatus; ///< An status number returned by the regex API.
+typedef PCRE2_SIZE RzRegexSize; ///< Size of a text or regex. This is the size measured in code width. For UTF-8: bytes.
+typedef ut32 RzRegexFlags; ///< Regex flag bits.
+typedef PCRE2_SPTR RzRegexPattern; ///< A regex pattern string.
+typedef pcre2_code RzRegex; ///< A regex expression.
+typedef pcre2_match_data RzRegexMatchData; ///< A regex match data from PCRE2
 
-RZ_API int rz_regex_exec(const RzRegex *preg, const char *string, size_t nmatch, RzRegexMatch __pmatch[], int eflags);
-RZ_API void rz_regex_free(RzRegex *);
-RZ_API int rz_regex_comp(RzRegex *, const char *, int);
-RZ_API size_t rz_regex_error(int, const RzRegex *, char *, size_t);
+typedef struct {
+	RzRegexSize mname_idx; ///< Match name index into the pattern name table.
+	RzRegexSize start; ///< Start offset into the text where the match starts.
+	RzRegexSize len; ///< Length of match in bytes.
+} RzRegexMatch;
 
-// Non native helper functions for our convenience.
-RZ_API RzRegex *rz_regex_new(const char *pattern, RzRegexFlags cflags);
+typedef pcre2_general_context RzRegexGeneralContext; ///< General context.
+typedef pcre2_compile_context RzRegexCompContext; ///< The context for compiling.
+typedef pcre2_match_context RzRegexMatchContext; ///< The context for matching.
+typedef struct {
+	RzRegexGeneralContext *general;
+	RzRegexCompContext *compile;
+	RzRegexMatchContext *match;
+} RzRegexContexts;
+
+RZ_API RZ_OWN RzRegex *rz_regex_new(const char *pattern, RzRegexFlags cflags, RZ_NULLABLE RzRegexCompContext *context);
+RZ_API RZ_OWN RzRegexMatchData *rz_regex_match_data_new(const RzRegex *regex, RzRegexGeneralContext *context);
 RZ_API void rz_regex_fini(RzRegex *);
-RZ_API int rz_regex_match(const char *pattern, const char *text, RzRegexFlags cflags, RzRegexFlags eflags);
-RZ_API const char *rz_regex_match_extract(RZ_NONNULL const char *text, RZ_NONNULL RzRegexMatch *match);
+RZ_API void rz_regex_error_msg(RzRegexStatus errcode, RZ_OUT char *errbuf, RzRegexSize errbuf_size);
+RZ_API void rz_regex_match_data_free(RZ_OWN RzRegexMatchData *match_data);
+RZ_API const ut8 *rz_regex_get_match_name(const RzRegex *regex, ut32 name_idx);
+RZ_API RZ_OWN RzVector /*<RzMatch>*/ *rz_regex_match_first(
+	const RzRegex *regex,
+	RZ_NONNULL const char *text,
+	RzRegexSize text_offset,
+	RzRegexFlags options,
+	RZ_NULLABLE RzRegexContexts *ctxs);
+RZ_API RZ_OWN RzPVector /*<RzVector>*/ *rz_regex_match_all(
+	const RzRegex *regex,
+	RZ_NONNULL const char *text,
+	RzRegexSize text_offset,
+	RzRegexFlags options,
+	RZ_NULLABLE RzRegexContexts *ctxs);
 
 #endif /* !_REGEX_H_ */
---
+++ librz/util/regex.c
@@ -0,0 +1,220 @@
+// SPDX-FileCopyrightText: 2023 Rot127 <unisono@quyllur.org>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include <rz_vector.h>
+#include <rz_regex.h>
+#include <rz_types.h>
+#include <rz_util/rz_assert.h>
+#include <rz_util.h>
+
+/**
+ * \file Defines the wrapper functions to PCRE2.
+ */
+
+/**
+ * \brief Compile a regex pattern to a RzRegex and return it.
+ * In case of an error, an error message is printed and NULL is returned.
+ *
+ * \param pattern The regex pattern string.
+ * \param cflags The compilation flags or zero for default.
+ * \param context The context for compilation. Can be NULL for default context.
+ *
+ * \return The compiled regex or NULL in case of failure.
+ */
+RZ_API RZ_OWN RzRegex *rz_regex_new(const char *pattern, RzRegexFlags cflags, RZ_NULLABLE RzRegexCompContext *context) {
+	RzRegexStatus err_num;
+	RzRegexSize err_off;
+	RzRegex *regex = pcre2_compile(
+		(PCRE2_SPTR)pattern,
+		PCRE2_ZERO_TERMINATED,
+		cflags,
+		&err_num,
+		&err_off,
+		context);
+	if (!regex) {
+		PCRE2_UCHAR buffer[256];
+		pcre2_get_error_message(err_num, buffer, sizeof(buffer));
+		RZ_LOG_ERROR("Regex compilation failed at offset %" PFMTSZu ": %s\n", err_off,
+			buffer);
+		return NULL;
+	}
+	return regex;
+}
+
+/**
+ * \brief Frees a given RzRegex.
+ *
+ * \param regex The RzRegex to free.
+ */
+RZ_API void rz_regex_fini(RZ_OWN RzRegex *regex) {
+	pcre2_code_free(regex);
+}
+
+RZ_API RZ_OWN RzRegexMatchData *rz_regex_match_data_new(const RzRegex *regex, RzRegexGeneralContext *context) {
+	return pcre2_match_data_create_from_pattern(regex, context);
+}
+
+RZ_API void rz_regex_match_data_free(RZ_OWN RzRegexMatchData *match_data) {
+	pcre2_match_data_free(match_data);
+}
+
+static RzRegexStatus rz_regex_match(const RzRegex *regex, RZ_NONNULL const char *text,
+	RzRegexSize text_offset,
+	RzRegexFlags options,
+	RZ_NONNULL RZ_BORROW RzRegexMatchData *match_data,
+	RZ_NULLABLE RzRegexMatchContext *mcontext) {
+	return pcre2_match(regex, (PCRE2_SPTR)text, PCRE2_ZERO_TERMINATED, text_offset, options | PCRE2_MATCH_INVALID_UTF, match_data, mcontext);
+}
+
+/**
+ * \brief Generates the error message to \p errcode.
+ *
+ * \param errcode The error code.
+ * \param errbuf The error message buffer.
+ * \param errbuf_size The error message buffer size in bytes.
+ */
+RZ_API void rz_regex_error_msg(RzRegexStatus errcode, RZ_OUT char *errbuf, RzRegexSize errbuf_size) {
+	pcre2_get_error_message(errcode, (PCRE2_UCHAR *)errbuf, errbuf_size);
+}
+
+RZ_API const ut8 *rz_regex_get_match_name(const RzRegex *regex, ut32 name_idx) {
+	rz_return_val_if_fail(regex, NULL);
+
+	ut32 namecount;
+	ut32 name_entry_size;
+	PCRE2_SPTR nametable_ptr;
+
+	pcre2_pattern_info(
+		regex,
+		PCRE2_INFO_NAMECOUNT,
+		&namecount);
+
+	pcre2_pattern_info(
+		regex,
+		PCRE2_INFO_NAMETABLE,
+		&nametable_ptr);
+
+	pcre2_pattern_info(
+		regex,
+		PCRE2_INFO_NAMEENTRYSIZE,
+		&name_entry_size);
+
+	for (size_t i = 0; i < namecount; i++) {
+		int n = (nametable_ptr[0] << 8) | nametable_ptr[1];
+		if (n == name_idx) {
+			return nametable_ptr + 2;
+		}
+		nametable_ptr += name_entry_size;
+	}
+	return NULL;
+}
+
+RZ_API RZ_OWN RzVector /*<RzMatch>*/ *rz_regex_match_first(
+	const RzRegex *regex,
+	RZ_NONNULL const char *text,
+	RzRegexSize text_offset,
+	RzRegexFlags options,
+	RZ_NULLABLE RzRegexContexts *ctxs) {
+	RzRegexMatchContext *mctx = NULL;
+	RzRegexGeneralContext *gctx = NULL;
+	if (ctxs) {
+		mctx = ctxs->match;
+		gctx = ctxs->general;
+	}
+
+	RzVector *matches = rz_vector_new(sizeof(RzRegexMatch), NULL, NULL);
+	RzRegexMatchData *mdata = pcre2_match_data_create_from_pattern(regex, gctx);
+	RzRegexStatus rc = rz_regex_match(regex, text, text_offset, options, mdata, mctx);
+
+	if (rc == PCRE2_ERROR_NOMATCH) {
+		// Nothing matched return empty vector.
+		rz_regex_match_data_free(mdata);
+		return matches;
+	}
+
+	if (rc < 0) {
+		// Some error happend. Inform the user.
+		PCRE2_UCHAR buffer[256];
+		pcre2_get_error_message(rc, buffer, sizeof(buffer));
+		RZ_LOG_WARN("Regex matching failed: %s\n", buffer);
+		goto error;
+	}
+
+	// Add groups to vector
+	PCRE2_SIZE *ovector = pcre2_get_ovector_pointer(mdata);
+
+	ut32 name_entry_size;
+	PCRE2_SPTR nametable_ptr;
+
+	pcre2_pattern_info(
+		regex,
+		PCRE2_INFO_NAMETABLE,
+		&nametable_ptr);
+
+	pcre2_pattern_info(
+		regex,
+		PCRE2_INFO_NAMEENTRYSIZE,
+		&name_entry_size);
+
+	for (size_t i = 0; i < rc; i++) {
+		if (ovector[2 * i] > ovector[2 * i + 1]) {
+			// This happens for \K lookaround. We fail if used.
+			// See pcre2demo.c for details.
+			RZ_LOG_ERROR("Usage of \\K to set start of the pattern later than the end, is not implemented.\n");
+			goto error;
+		}
+
+		// Offset and length of match
+		RzRegexMatch *match = RZ_NEW0(RzRegexMatch);
+		match->start = ovector[2 * i];
+		match->len = ovector[2 * i + 1] - match->start;
+
+		// Match index with a name.
+		// Index is saved in the first two bytes of a table entry.
+		ut32 n = (nametable_ptr[0] << 8) | nametable_ptr[1];
+		if (n != i) {
+			// No name
+			match->mname_idx = RZ_REGEX_UNSET;
+			rz_vector_push(matches, match);
+			continue;
+		}
+
+		match->mname_idx = n;
+		nametable_ptr += name_entry_size;
+		rz_vector_push(matches, match);
+	}
+
+	rz_regex_match_data_free(mdata);
+	return matches;
+
+error:
+	rz_regex_match_data_free(mdata);
+	rz_vector_free(matches);
+	return NULL;
+}
+
+/**
+ * \brief Finds all matches in a text and returns them as vector.
+ */
+RZ_API RZ_OWN RzPVector /*<RzVector>*/ *rz_regex_match_all(
+	const RzRegex *regex,
+	RZ_NONNULL const char *text,
+	RzRegexSize text_offset,
+	RzRegexFlags options,
+	RZ_NULLABLE RzRegexContexts *ctxs) {
+	rz_return_val_if_fail(regex && text, NULL);
+
+	RzPVector *all_matches = rz_pvector_new((RzPVectorFree)rz_vector_free);
+	RzVector *matches = rz_regex_match_first(regex, text, text_offset, options, ctxs);
+	while (matches && rz_vector_len(matches) > 0) {
+		rz_pvector_push(all_matches, matches);
+		RzRegexMatch *m = rz_vector_head(matches);
+		// Search again after the last match.
+		text_offset = m->start + m->len;
+		matches = rz_regex_match_first(regex, text, text_offset, options, ctxs);
+	}
+
+	// Free last vector without matches.
+	rz_vector_free(matches);
+	return all_matches;
+}
