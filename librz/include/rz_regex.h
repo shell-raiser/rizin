fb5e2d5431138f164f0b8656b1d58c918951156c
Rot127 <unisono@quyllur.org>
2024-01-31 05:15:22 -0500

    Set static flag since we only build it static.

--- librz/include/rz_regex.h
+++ librz/include/rz_regex.h
@@ -10,8 +10,7 @@
 #include <rz_list.h>
 #include <sys/types.h>
 
-// TODO Check for static windows build and define this
-/* #define PCRE2_STATIC */
+#define PCRE2_STATIC
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 
