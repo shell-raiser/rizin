8e2314595d6b54e0ac7ad191204cdf1bfeb622e6
Rot127 <unisono@quyllur.org>
2024-02-01 00:11:28 -0500

    Run clang-format

--- librz/util/regex.c
+++ librz/util/regex.c
@@ -343,7 +343,7 @@ RZ_API RZ_OWN RzStrBuf *rz_regex_full_match_str(const char *pattern, const char 
 		// No separator in case of only one match
 		if (rz_pvector_len(matches) == 1 && !rz_strbuf_appendf(sbuf, "%-.*s", (int)match->len, t)) {
 			goto fini;
-		}	else if (!rz_strbuf_appendf(sbuf, "%-.*s%s", (int)match->len, t, separator)) {
+		} else if (!rz_strbuf_appendf(sbuf, "%-.*s%s", (int)match->len, t, separator)) {
 			goto fini;
 		}
 	}
